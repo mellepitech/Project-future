bf91c731d3e288869a05ea5fa8dc248a00ae10f3
mellGitson <mell.canac@epitech.eu>
2024-08-26 11:22:59 +0200

    aaaaa

---
+++ my-app/src/components/nav/SideNavbar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+import { useState } from "react";
+
+import "./SideNavbar.css";
+
+export const SideNavbar = () => {
+  const [activeNav, setActiveNav] = useState('#');
+
+  return (
+    <nav>
+      <a href="#" 
+        onClick={() => setActiveNav('#')}
+        className={activeNav === '#' ? 'active' : ''}
+      >
+        <Typography>
+          Home
+        </Typography>
+      </a>
+      <a href="#works"
+        onClick={() => setActiveNav('#works')}
+        className={activeNav === '#works' ? 'active' : ''}
+      >
+        <Typography>
+          Projects
+        </Typography>
+      </a>
+      <a href="#about" 
+        onClick={() => setActiveNav('#about')}
+        className={activeNav === '#about' ? 'active' : ''}
+      >
+        <Typography>
+          About
+        </Typography>
+      </a>
+      <a href="#contact"
+        onClick={() => setActiveNav('#contact')}
+        className={activeNav === '#contact' ? 'active' : ''}
+      >
+        <Typography>
+          Contact
+        </Typography>
+      </a>
+    </nav>
+  );
+};
